6d61fc85ad70ac3e14b842a3a77af5beb055f83a
Steven Aiello <sma92878@gmail.com>
2019-05-26 23:04:51 -0400

    Added a fixed amount of page options moving up and down

--- index.js
+++ index.js
@@ -37,10 +37,31 @@ let items = [
     {name : "Item 36", size : "Medium", price : 9.99},
     {name : "Item 37", size : "Small", price : 19.99},
     {name : "Item 38", size : "Medium", price : 39.99},
+    {name : "Item 39", size : "Large", price : 79.99},
+    {name : "Item 20", size : "Medium", price : 19.99},
+    {name : "Item 21", size : "Medium", price : 19.99},
+    {name : "Item 22", size : "Small", price : 29.99},
+    {name : "Item 23", size : "Large", price : 19.99},
+    {name : "Item 24", size : "Medium", price : 39.99},
+    {name : "Item 25", size : "Small", price : 49.99},
+    {name : "Item 26", size : "Medium", price : 9.99},
+    {name : "Item 27", size : "Small", price : 19.99},
+    {name : "Item 28", size : "Medium", price : 39.99},
+    {name : "Item 29", size : "Large", price : 79.99},
+    {name : "Item 30", size : "Medium", price : 19.99},
+    {name : "Item 31", size : "Medium", price : 19.99},
+    {name : "Item 32", size : "Small", price : 29.99},
+    {name : "Item 33", size : "Large", price : 19.99},
+    {name : "Item 34", size : "Medium", price : 39.99},
+    {name : "Item 35", size : "Small", price : 49.99},
+    {name : "Item 36", size : "Medium", price : 9.99},
+    {name : "Item 37", size : "Small", price : 19.99},
+    {name : "Item 38", size : "Medium", price : 39.99},
     {name : "Item 39", size : "Large", price : 79.99}
 ]
 
 let numberToDisplay = 5;
+let currentPage = 1;
 
 let itemList = (start, numberToDisplay) => {
     let itemDiv = document.getElementById('items') 
@@ -58,8 +79,10 @@ let itemList = (start, numberToDisplay) => {
     document.getElementById('items').appendChild(ul);
 }
 
-let pagination = () => {
+let pagination = (pageToStart = 1) => {
+    currentPage = pageToStart;
     let pagination = document.getElementById('pagination')
+    pagination.innerHTML = ""
     let pages = items.length / numberToDisplay
     pages = Math.ceil(pages)
     let ul = document.createElement('ul')
@@ -68,17 +91,29 @@ let pagination = () => {
     first.innerText = "First"
     first.addEventListener('click', function(){
         itemList(0, numberToDisplay)
+        jumpToFirst()
     })
     
     ul.appendChild(first)
 
-    for(let i = 1; i <= pages; i++ ){
+    let i = pageToStart;
+    let pageToEnd = pageToStart + 4
+
+    if(pageToEnd > pages){
+        i = pages - 4
+        pageToEnd = pages
+    }
+
+    for(i; i <= pageToEnd; i++ ){        
         let li = document.createElement('li')
         li.innerText = i
         li.setAttribute('id', (i - 1))
         li.addEventListener('click', function(event){
             let itemToStart = event.target.id * numberToDisplay 
             itemList(itemToStart, numberToDisplay)
+            
+            buttonClicked = Number(event.target.id) + Number(1)
+            updatePagination(buttonClicked)
         })
         ul.appendChild(li)
     }
@@ -88,10 +123,27 @@ let pagination = () => {
     last.addEventListener('click', function(){
         let itemToStart = (pages - 1) * numberToDisplay
         itemList(itemToStart, numberToDisplay)
+        updatePagination(pages)
     })
 
     ul.appendChild(last)
     pagination.appendChild(ul)
+}
+
+let jumpToFirst = () => {
+    pagination(1)
+}
+
+let updatePagination = (pageStart) =>{
+    if(pageStart < currentPage || pageStart == currentPage){
+        pageStart = pageStart - 2
+    }
+
+    if(pageStart < 1){
+        pageStart = 1
+    }
+    let pageEnd = pageStart + 4
+    pagination(pageStart, pageEnd)
 }
 
 itemList(0, numberToDisplay);
